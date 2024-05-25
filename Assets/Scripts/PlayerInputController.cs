563e0854733b1bcaba0f6ab12797e2492d8193a8
OneRiver98 <167278772+OneRiver98@users.noreply.github.com>
2024-05-26 00:20:50 +0900

    [24.05.26]AddPlayerMove, FixTileMap
    캐릭터움직임 Script 추가. 타일맵 살짝 수정.

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInputValue = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInputValue);
+    }
+}
---
+++ Assets/Scripts/TopDownMovement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class TopDownMovement : MonoBehaviour
+{
+    private TopDownController movementController;
+    private Rigidbody2D movementRigidbody;
+
+    private Vector2 movementDirection = Vector2.zero;
+    private void Awake()
+    {
+        movementController = GetComponent<TopDownController>();
+        movementRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        movementController.OnMoveEvent += Move;
+    }
+
+    private void Move(Vector2 direction)
+    {
+        movementDirection = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(movementDirection);
+    }
+
+    private void ApplyMovement(Vector2 direction)
+    {
+        direction = direction * 5;
+
+        movementRigidbody.velocity = direction;
+    }
+}
