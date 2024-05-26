6b9c33354f1254d1ad4f2712f3269aae1ab92fc7
OneRiver98 <167278772+OneRiver98@users.noreply.github.com>
2024-05-26 18:58:11 +0900

    [24.05.26]AddCameraMove
    플레이어 위치에 따라서 카메라움직임 구현

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera mainCamera;
+    [SerializeField]private Transform cameraTarget;
+
+
+    private void Awake()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = new Vector3(cameraTarget.position.x, cameraTarget.position.y, -10);
+        mainCamera.transform.position = cameraPosition;
+    }
+}
